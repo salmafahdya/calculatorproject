58d2a4fa92ecf9dc08a106e4290c8289691195a3
salma sayed <salmakhaled2205@gmail.com>
2024-02-03 23:02:33 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+class Calculator {
+    constructor (previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear () {
+      this.currentOperand = ''
+      this.previousOperand = ''
+      this.operation = undefined
+    }
+    delete () {}
+    appendNumber (number) {
+      this.currentOperand = number
+    }
+    chooseOperation (operation) {}
+    compute () {}
+    updateDisplay () {
+      this.currentOperandTextElement.innerText = this.currentOperand
+    }
+  }
+  
+  const numberButtons = document.querySelectorAll('[data-number]')
+  const operationButtons = document.querySelectorAll('[data-operator]')
+  const previousOperandTextElement = document.querySelectorAll('[data-previous]')
+  const currentOperandTextElement = document.querySelectorAll('[data-current]')
+  const deletionButtons = document.querySelectorAll('[data-delete]')
+  const equalsButtons = document.querySelectorAll('[data-equal]')
+  const allClearButtons = document.querySelectorAll('[data-ac]')
+  const calculator = new Calculator(
+    previousOperandTextElement,
+    currentOperandTextElement
+  )
+  numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+  
