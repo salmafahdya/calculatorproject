474585f5c4f13a634fb6ef4ddff5680d06fbf9ba
salma khaled <you@example.com>
2024-02-03 19:53:52 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Salma from './components/Salma'
+
+function App () {
+  return (
+    <>
+      <h1>hello Salma ❤️❤️❤️</h1>
+      <Salma />
+    </>
+  )
+}
+
+export default App
